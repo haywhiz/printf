5e04ec0403c2a11c3854b6deab67e43e36aabfb6
haywhiz <54290706+haywhiz@users.noreply.github.com>
2022-05-25 19:41:25 +0100

    Update main.h

--- main.h
+++ main.h
@@ -1,27 +1,46 @@
-#ifndef _PRINTF
-#define _PRINTF
+#ifndef PRINT_F
+#define PRINT_F
 
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdarg.h>
 
 /**
- * struct convert - defines a structure foor specificers and function
- *
- * @ops: the operator
- * @f: function
+* struct convert - defines a structure for symbols and functions
+*
+* @sym: The operator
+* @f: The function associated
 */
-
 struct convert
 {
-	char *ops;
+	char *sym;
 	int (*f)(va_list);
 };
-typedef struct convert _convert;
+typedef struct convert conver_t;
 
-/* parser fuction is to count the number of characters printed*/
-int parser(const char *format, _convert f_list[], va_list arg_list);
-/* main function*/
-int _print(const char *format, ...);
+/*Main functions*/
+int parser(const char *format, conver_t f_list[], va_list arg_list);
+int _printf(const char *format, ...);
 int _write_char(char);
+int print_char(va_list);
+int print_string(va_list);
+int print_percent(va_list);
+int print_integer(va_list);
+int print_number(va_list);
+int print_binary(va_list);
+int print_reversed(va_list arg);
+int rot13(va_list);
+int unsigned_integer(va_list);
+int print_octal(va_list list);
+int print_hex(va_list list);
+int print_heX(va_list list);
 
+/*Helper functions*/
+unsigned int base_len(unsigned int, int);
+char *rev_string(char *);
+void write_base(char *str);
+char *_memcpy(char *dest, char *src, unsigned int n);
+int print_unsgined_number(unsigned int);
+
+
+#endif
