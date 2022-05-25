52b99ef4c6f8a788b9a7aaf4cdf8606d887dad9e
Haywhiz <babarindeademola@gmail.com>
2022-05-25 16:17:21 -0700

    new commit

--- _printf.c
+++ _printf.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * _printf - Receives the main string and all the necessary parameters to
- * print a formated string
- * @format: A string containing all the desired characters
- * Return: A total count of the characters printed
+ *  _printf - prints %c and %s format specifier
+ *  @format: format string
+ *  Return: number of printed charaters
  */
 
 int _printf(const char *format, ...)
 
 {
+<<<<<<< HEAD
 	int printed_chars;
 
 	conver_t f_list[] = {
@@ -50,4 +50,44 @@ int _printf(const char *format, ...)
 	printed_chars = parser(format, f_list, arg_list);
 	va_end(arg_list);
 	return (printed_chars);
+=======
+	unsigned int i = 0, count = 0;
+	va_list valist;
+	int (*f)(va_list);
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(valist, format);
+
+	while (format[i])
+	{
+		for (; format[i] != '%' && format[i]; i++)
+		{
+			_putchar(format[i]);
+			count++;
+		}
+
+		if (!format[i])
+			return (count);
+
+		f = check_specifier(&format[i + 1]);
+		if (f != NULL)
+		{
+			count += f(valist);
+			i += 2;
+			continue;
+		}
+		if (!format[i + 1])
+			return (-1);
+		_putchar(format[i]);
+		count++;
+		if (format[i + 1] == '%')
+			i += 2;
+		else
+			i++;
+	}
+	va_end(valist);
+	return (count);
+>>>>>>> 2b48ab73094749fad34d27b5c8ec7e7873e6dc48
 }
--- main.h
+++ main.h
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #ifndef PRINT_F
 
 #define PRINT_F
@@ -8,9 +9,18 @@
 
 #include <stdlib.h>
 
+=======
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+>>>>>>> 2b48ab73094749fad34d27b5c8ec7e7873e6dc48
 #include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 
 /**
+<<<<<<< HEAD
  * struct convert - defines a structure for symbols and functions
  * @sym: The operator
  * @f: The function associated
@@ -75,7 +85,38 @@ void write_base(char *str);
 char *_memcpy(char *dest, char *src, unsigned int n);
 
 int print_unsgined_number(unsigned int);
+=======
+ * struct print_struct - structure for printing various types
+ * @t: type to print
+ * @f: function to print
+ */
 
+typedef struct print_struct
+{
+	char *t;
+	int (*f)(va_list);
+} print_type;
+
+>>>>>>> 2b48ab73094749fad34d27b5c8ec7e7873e6dc48
+
+int _printf(const char *format, ...);
+int _putchar(char c);
+int print_c(va_list c);
+int print_s(va_list s);
+int print_i(va_list i);
+int print_d(va_list d);
+int print_b(va_list b);
+int print_u(va_list u);
+int print_o(va_list o);
+int print_x(va_list x);
+int print_X(va_list X);
+int print_S(va_list S);
+int print_p(va_list p);
+int (*check_specifier(const char *format))(va_list);
+int print_hex(unsigned int n, unsigned int c);
+int hex_print(char c);
+int print_r(va_list r);
+int print_R(va_list R);
 
 
 
