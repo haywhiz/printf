3fc1777092a90587937e2b2bb9e47f71b949de65
haywhiz <54290706+haywhiz@users.noreply.github.com>
2022-05-25 19:40:27 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,19 +1,19 @@
 #include "main.h"
 
 /**
- *_printf - receives the main string and all necessary parameters to
- * print a formatted string
+ * _printf - Receives the main string and all the necessary parameters to
+ * print a formated string
  * @format: A string containing all the desired characters
  * Return: A total count of the characters printed
-*/
+ */
 int _printf(const char *format, ...)
 {
 	int printed_chars;
-	_convert f_list[] = {
+	conver_t f_list[] = {
 		{"c", print_char},
 		{"s", print_string},
 		{"%", print_percent},
-		{"d", print_double},
+		{"d", print_integer},
 		{"i", print_integer},
 		{"b", print_binary},
 		{"r", print_reversed},
@@ -30,6 +30,7 @@ int _printf(const char *format, ...)
 		return (-1);
 
 	va_start(arg_list, format);
+	/*Calling parser function*/
 	printed_chars = parser(format, f_list, arg_list);
 	va_end(arg_list);
 	return (printed_chars);
